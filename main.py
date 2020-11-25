24c12882445356d562aaff4ade7fe28d4790d7ee
0-122-5 <0-122-5>
2020-11-25 17:43:34 +0500

    Second commit

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 import random
 
-from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from UI import Ui_Form
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -31,7 +32,7 @@ class MyWidget(QMainWindow):
         x = random.randint(70, 430)
         y = random.randint(40, 470)
         diametr = random.randint(5, 90)
-        qp.setBrush(QColor(255, 203, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         qp.drawEllipse(x, y, diametr, diametr)
 
 
